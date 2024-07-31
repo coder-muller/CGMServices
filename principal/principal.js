d40b17cd5df9f2fd67320676517ff8dc31346f57
Guilherme Muller <guilhermemullerxx@gmail.com>
2024-07-31 13:41:09 -0300

    Create Logout

--- principal/principal.js
+++ principal/principal.js
@@ -4,12 +4,14 @@ const btAgenda = document.getElementById('btAgenda');
 const btClientes = document.getElementById('btClientes');
 const btResultados = document.getElementById('btResultados');
 const btAjustes = document.getElementById('btAjustes');
+const btLogout = document.getElementById('btLogout');
 
 
 btAgenda.addEventListener('click', () => carregarConteudoHTML('../agenda/agenda.html'));
 btClientes.addEventListener('click', () => carregarConteudoHTML('../clientes/clientes.html'));
 btResultados.addEventListener('click', () => carregarConteudoHTML('../resultados/resultados.html'));
 btAjustes.addEventListener('click', () => carregarConteudoHTML('../ajustes/ajustes.html'));
+btLogout.addEventListener('click', logOut);
 
 function carregarConteudoHTML(pagina) {
     const iframe = document.createElement('iframe');
@@ -19,6 +21,14 @@ function carregarConteudoHTML(pagina) {
     iframe.style.border = 'none';
     conteudo.innerHTML = '';
     conteudo.appendChild(iframe);
+}
+
+function logOut() {
+    localStorage.removeItem('chaveConectada')
+    localStorage.removeItem('idConectado')
+    localStorage.removeItem('usuarioConectado')
+    localStorage.removeItem('permissaoConectada')
+    window.location.href = '../index.html'
 }
 
 document.addEventListener('DOMContentLoaded', () => carregarConteudoHTML('../agenda/agenda.html'));
