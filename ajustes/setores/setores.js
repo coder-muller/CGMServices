f518a48e0e8165f034ae621ff5ca6f9970363b34
Guilherme Muller <guilhermemullerxx@gmail.com>
2024-08-22 18:52:55 -0300

    ajustes

--- ajustes/setores/setores.js
+++ ajustes/setores/setores.js
@@ -1,3 +1,5 @@
+import { sendDelete, sendGet, sendPost, sendPut, toggleModal } from "../../utils.js"
+
 const modal = document.getElementById('modal')
 const form = document.getElementById('formSetores')
 const btNovo = document.getElementById('novoSetor')
@@ -13,14 +15,14 @@ let isEdit = false;
 let editId = null;
 
 function AbrirModal(mode, setor = '') {
-    modal.style.display = 'block'
+    toggleModal('modal', true)
     isEdit = (mode === 1);
     setorInput.value = setor;
     setorInput.focus()
 }
 
 function FecharModal() {
-    modal.style.display = 'none'
+    toggleModal('modal', false)
     form.reset()
     isEdit = false;
     editId = null;
@@ -36,74 +38,59 @@ document.addEventListener('DOMContentLoaded', async function () {
 
 async function carregarSetores() {
     const chave = localStorage.getItem('chaveConectada');
-    try {
-        const response = await fetch('http://localhost:4567/setores/' + chave);
-        if ((await response).ok) {
-            const data = await response.json();
-            while (table.rows.length > 1) {
-                table.deleteRow(1);
-            }
-            for (let i = 0; i < data.length; i++) {
-                const item = data[i];
-                const row = tableTbody.insertRow();
-                row.onclick = function () {
-                    AbrirModal(1, item.setor);
-                    editId = item.id;
-                };
-                const cell1 = row.insertCell(0);
-                const cell2 = row.insertCell(1);
-                const cell3 = row.insertCell(2);
-                cell1.innerHTML = item.id;
-                cell2.innerHTML = item.setor;
-                const trashIcon = document.createElement('img');
-                trashIcon.src = "../../img/trash.png";
-                trashIcon.alt = "excluir";
-                trashIcon.onclick = function (event) {
-                    event.stopPropagation();
-                    Deletar(item.id);
-                };
-                cell3.appendChild(trashIcon);
-            }
-        } else {
-            alert('Erro ao carregar setores!');
-            console.log(response.json());
+    const response = await sendGet('/setores/' + chave);
+    if (response) {
+        const data = response;
+        while (table.rows.length > 1) {
+            table.deleteRow(1);
         }
-    } catch (error) {
-        console.log(error);
+        for (let i = 0; i < data.length; i++) {
+            const item = data[i];
+            const row = tableTbody.insertRow();
+            row.onclick = function () {
+                AbrirModal(1, item.setor);
+                editId = item.id;
+            };
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            const cell3 = row.insertCell(2);
+            cell1.innerHTML = item.id;
+            cell2.innerHTML = item.setor;
+            const trashIcon = document.createElement('img');
+            trashIcon.src = "../../img/trash.png";
+            trashIcon.alt = "excluir";
+            trashIcon.onclick = function (event) {
+                event.stopPropagation();
+                Deletar(item.id);
+            };
+            cell3.appendChild(trashIcon);
+        }
+    } else {
         alert('Erro ao carregar setores!');
+        console.log(response.json());
     }
 }
 
 async function Cadastrar(event) {
     event.preventDefault();
+    let response = null;
     if (form.checkValidity()) {
-        try {
-            const setor = setorInput.value
-            const chave = localStorage.getItem('chaveConectada')
-            const usuario = localStorage.getItem('usuarioConectado')
-            const data = {
-                chave,
-                setor,
-                usuario,
-            }
-            const method = isEdit ? 'PUT' : 'POST';
-            const url = isEdit ? 'http://localhost:4567/setores/' + editId : 'http://localhost:4567/setores';
-            const response = await fetch(url, {
-                method: method,
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-            if (response.ok) {
-                carregarSetores()
-                modal.style.display = 'none'
-            } else {
-                alert('Erro ao cadastrar/editar setor no banco de dados!')
-                modal.style.display = 'none'
-            }
-        } catch (error) {
-            console.error(error)
+        const setor = setorInput.value
+        const chave = localStorage.getItem('chaveConectada')
+        const usuario = localStorage.getItem('usuarioConectado')
+        const data = { chave, setor, usuario, }
+
+        if (isEdit) {
+            response = await sendPut('/setores/' + editId, data)
+        } else {
+            response = await sendPost('/setores', data)
+        }
+        if (response) {
+            carregarSetores()
+            toggleModal('modal', false)
+        } else {
+            alert('Erro ao cadastrar/editar setor no banco de dados!')
+            toggleModal('modal', false)
         }
     } else {
         form.reportValidity()
@@ -113,25 +100,15 @@ async function Cadastrar(event) {
 async function Deletar(id) {
     const confirmacao = confirm('Você tem certeza que deseja apagar esse setor?')
     if (confirmacao) {
-        try {
-            const usuario = localStorage.getItem('usuarioConectado')
-            const data = {
-                usuario,
-            }
-            const response = await fetch('http://localhost:4567/setores/' + id, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-            if (response.ok) {
-                carregarSetores()
-            } else {
-                alert('Erro ao deletar setor no banco de dados!')
-            }
-        } catch (error) {
-            console.error(error)
+        const usuario = localStorage.getItem('usuarioConectado')
+        const data = {
+            usuario,
+        }
+        const response = await sendDelete('/setores/' + id, data)
+        if (response) {
+            carregarSetores()
+        } else {
+            alert('Erro ao deletar setor no banco de dados!')
         }
     }
 }
